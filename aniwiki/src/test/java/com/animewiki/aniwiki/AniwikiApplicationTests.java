41471de502c51896136e9e7f9042fe5e94ef9c21
Rogelio Yael Hernandez Jaimes <roger_libra01@hotmail.com>
2025-11-04 11:36:31 -0600

    Init project

---
+++ aniwiki/src/test/java/com/animewiki/aniwiki/AniwikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.animewiki.aniwiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AniwikiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
